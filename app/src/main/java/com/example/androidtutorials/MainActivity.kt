d8a9dd8d1b42a44215f9e499926d8a9f67c0a7d9
PawelPrzytula <pawel.przytula@m2t.com.pl>
2023-09-10 22:19:22 +0200

    feat: add simple form

---
+++ app/src/main/java/com/example/androidtutorials/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.androidtutorials
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.androidtutorials.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view  = binding.root
+        setContentView(view)
+        
+
+
+    }
+    fun printResult(view: View){
+        when (view.id){
+            R.id.btnApply ->{
+                val firstName = binding.etFirstName.text.toString()
+                val lastName = binding.etLastName.text.toString()
+                val birthDate = binding.etBirthDate.text.toString()
+                val country = binding.etCountry.text.toString()
+
+                binding.tvSummary.text = "First Name: $firstName\n" +
+                        "Last Name: $lastName\n" +
+                        "Birth Date: $birthDate\n" +
+                        "Country: $country"
+            }
+        }
+    }
+}
